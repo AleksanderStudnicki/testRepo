1bf1f5029e935ff38888f946ed55561076f15946
AleksanderStudnicki <alek.studnicki@gmail.com>
2019-12-08 23:01:28 +0100

    Initial commit of project generated by mantra script

---
+++ src/main/java/app/studnicki/testRepo/App.java
@@ -0,0 +1,13 @@
+package app.studnicki.testRepo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
